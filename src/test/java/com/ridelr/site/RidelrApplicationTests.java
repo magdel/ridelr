86637e7051aaee512eb11b8efc0aa89e8aa8574c
MagDel <pavelrv@yandex.ru>
2017-04-20 23:44:15 +0400

    basic tests

--- src/test/java/com/ridelr/site/RidelrApplicationTests.java
+++ src/test/java/com/ridelr/site/RidelrApplicationTests.java
@@ -1,23 +1,70 @@
 package com.ridelr.site;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
 @SpringBootTest(classes = {RidelrApplication.class})
 public class RidelrApplicationTests extends AbstractTestNGSpringContextTests {
+    private static final Logger LOG = LoggerFactory.getLogger(RidelrApplicationTests.class);
 
     @Autowired
-    Environment environment;
+    private Environment environment;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
 
     @BeforeClass
-    public void setupd() {
+    public void setupBeforeClass() {
+        LOG.info("setupBeforeClass()");
         System.out.println("server.port=" + environment.getProperty("server.port"));
+        this.mockMvc = webAppContextSetup(this.wac).build();
+        LOG.info("Setup OK");
     }
+
+    @Test
+    public void testHome() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
+
+    @Test
+    public void testAbout() throws Exception {
+        mockMvc.perform(get("/about"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("about"));
+    }
+
+    @Test
+    public void testContact() throws Exception {
+        mockMvc.perform(get("/contact"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("contact"));
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
 
     @Test
     public void testContext() throws Exception {
